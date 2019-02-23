7b4a0dae32a1a76deaccb2aee5376df1127cf627
Amobin <rayanjackson417@gmail.com>
2019-02-23 17:16:07 +0600

    added for loop with even number calculation

--- EvenOddNumber.py
+++ EvenOddNumber.py
@@ -1,5 +1,9 @@
 a=int(input("Enter the number of a:"))
 if(a%2==0):
-    print("is Even number",a)
+ 0   print("is Even number",a)
 else:
     print("is Odd number",a)
+
+for i in range(100):
+	if ( i%2==0):
+		print(i)
